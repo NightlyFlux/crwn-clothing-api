f097eb6961bffae589c6d85a9c0c28db8b95d143
Salvador G <nightlyflux@gmail.com>
2021-02-18 09:28:10 -0700

    Initial Commit

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const path = require('path')
+const compression = require('compression')
+
+if (process.env.NODE_ENV !== 'production') require('dotenv').config()
+
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
+
+const app = express()
+const port = process.env.PORT || 5000
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors())
+app.use(compression())
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+  )
+}
+
+app.listen(port, (error) => {
+  if (error) throw error
+  console.log(`crwn-server listening on port ${port}!`)
+})
+
+app.post('/payment', (req, res) => {
+  const body = {
+    amount: req.body.amount,
+    currency: 'USD',
+    source: req.body.token.id,
+    description: 'Esto es prueba',
+  }
+  console.log(req.body)
+  stripe.charges.create(body, (stripeRes, stripeErr) => {
+    if (stripeErr) {
+      res.status(500).send({ error: stripeErr })
+    } else {
+      res.status(200).send({ success: stripeRes })
+    }
+  })
+})
